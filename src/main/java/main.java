bab2d7f2d4f1ee1daf1eba241c98531465474eb2
OikonomouEvangelos <oikonomouevaggelos35@gmail.com>
2025-11-08 15:04:37 +0200

    addition

---
+++ src/main/java/main.java
@@ -0,0 +1,13 @@
+public class main{
+    public static void main(String[] args) {
+        // Δήλωση δύο αριθμών
+        int a = 5;
+        int b = 7;
+
+        // Υπολογισμός αθροίσματος
+        int sum = a + b;
+
+        // Εμφάνιση αποτελέσματος
+        System.out.println("Το άθροισμα των " + a + " και " + b + " είναι: " + sum);
+    }
+}
